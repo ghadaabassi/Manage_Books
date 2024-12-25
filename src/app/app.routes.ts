631ac86de0f8e998df4572bd7573f2abac711239
ghada <gh.abassi01@gmail.com>
2024-12-25 15:22:10 +0100

    AboutUs.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from '../components/home/home.component';
 import { ThreeDModelComponent } from '../components/three-dmodel/three-dmodel.component';
 import { FreeBooksComponent } from '../components/free-books/free-books.component';
 import { BookRecommenderComponent } from '../components/book-recommender/book-recommender.component';
+import { AboutUsComponent } from '../components/about-us/about-us.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'model', component: ThreeDModelComponent },
   { path: 'free', component: FreeBooksComponent },
   { path: 'recommender', component: BookRecommenderComponent },
+  { path: 'aboutUs', component: AboutUsComponent },
 ];
