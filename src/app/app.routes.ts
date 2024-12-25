2f5d2b17c0caebac6b59d6a0ef62eab7df78d3b0
ghada <gh.abassi01@gmail.com>
2024-12-25 15:33:02 +0100

    updateRouting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ThreeDModelComponent } from '../components/three-dmodel/three-dmodel.co
 import { FreeBooksComponent } from '../components/free-books/free-books.component';
 import { BookRecommenderComponent } from '../components/book-recommender/book-recommender.component';
 import { AboutUsComponent } from '../components/about-us/about-us.component';
+import { SignInComponent } from '../components/authentication/sign-in/sign-in.component';
+import { SignUpComponent } from '../components/authentication/sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -11,4 +13,6 @@ export const routes: Routes = [
   { path: 'free', component: FreeBooksComponent },
   { path: 'recommender', component: BookRecommenderComponent },
   { path: 'aboutUs', component: AboutUsComponent },
+  { path: 'SignIn', component: SignInComponent },
+  { path: 'SignUp', component: SignUpComponent },
 ];
