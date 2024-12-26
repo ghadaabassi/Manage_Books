4d00cb1db827829b5fcab7197a05d5e217813ecb
ghada <gh.abassi01@gmail.com>
2024-12-26 13:18:29 +0100

    SignUp

--- src/components/authentication/sign-up/sign-up.component.ts
+++ src/components/authentication/sign-up/sign-up.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   imports: [],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-
+  constructor(private router: Router) {}
+  onSubmit(): void {
+    this.router.navigate(['/model']).then(() => {
+      setTimeout(() => {
+        this.router.navigate(['/aboutUs']);
+      }, 7000);
+    });
+  }
 }
