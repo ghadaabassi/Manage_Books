c29d024d8c49ffcf6aed23443775acf215ce2928
ghada <gh.abassi01@gmail.com>
2024-12-31 23:55:15 +0100

    addUserAccount

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { AboutUsComponent } from '../components/about-us/about-us.component';
 import { SignInComponent } from '../components/authentication/sign-in/sign-in.component';
 import { SignUpComponent } from '../components/authentication/sign-up/sign-up.component';
 import { NotfoundComponent } from '../components/notfound/notfound.component';
+import { UserAccountComponent } from '../components/user-account/user-account.component';
+
 import { roleGuard } from '../guards/role.guard';
 
 export const routes: Routes = [
@@ -22,5 +24,6 @@ export const routes: Routes = [
   { path: 'aboutUs', component: AboutUsComponent },
   { path: 'signIn', component: SignInComponent },
   { path: 'signup', component: SignUpComponent },
+  { path: 'account', component: UserAccountComponent },
   { path: '**', component: NotfoundComponent },
 ];
