227fe497fdcfd3b7bfd57d10617a911e83623e3b
ghada <gh.abassi01@gmail.com>
2024-12-31 22:12:34 +0100

    implementRoleGuard

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,5 +1,18 @@
 import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 export const roleGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const requiredRole = route.data['role'];
+
+  if (authService.hasRole(requiredRole)) {
+    return true;
+  } else {
+    router.navigate(['/not-authorized']);
+    return false;
+  }
 };
