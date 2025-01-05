8c56787b886ce32dd5bfe99a7bef995925ba2241
ghada <gh.abassi01@gmail.com>
2025-01-05 11:31:46 +0100

    improvments

---
+++ src/components/book-collection/book-collection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-collection',
+  imports: [],
+  templateUrl: './book-collection.component.html',
+  styleUrl: './book-collection.component.css'
+})
+export class BookCollectionComponent {
+
+}
